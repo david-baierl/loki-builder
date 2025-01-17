98327b7b62a963e3857223dcc8d9919f0069ded0
David Baierl <12718257+david-baierl@users.noreply.github.com>
2025-01-17 10:28:38 +0000

    vite: fixed lenaria crashing in some TS cases by adding babel TS preset

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,12 @@ const host = process.env.TAURI_DEV_HOST
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [
-    linaria({ include: ['**/*.{ts,tsx}'] }),
+    linaria({
+      include: ['**/*.{ts,tsx}'],
+      babelOptions: {
+        presets: ['@babel/typescript'],
+      },
+    }),
     solid(),
   ],
 
