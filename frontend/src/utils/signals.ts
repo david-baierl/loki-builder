c45ee82849711d3905c532f71be148fabb319f83
David Baierl <12718257+david-baierl@users.noreply.github.com>
2025-01-17 10:26:37 +0000

    utils/signals: extend jsdoc comments

--- frontend/src/utils/signals.ts
+++ frontend/src/utils/signals.ts
@@ -16,16 +16,21 @@ import {
 // ---------------------------------------------
 
 export type SignalGetter<T> = Accessor<T> & {
+
+  /** @hint this is tracked */
   get: Accessor<T>
+
+  /** @hint this is untracked */
   peek: Accessor<T>
 
+  /** @hint this is tracked */
   readonly value: T
 }
 
 export type SignalSetter<T> = Setter<T> & {
   set: Setter<T>
 
-  /** @warn this is untracked */
+  /** @hint this is untracked */
   value: T
 }
 
@@ -38,7 +43,21 @@ export type Signal<T> = (
   & {
     get: SignalGetter<T>
     set: SignalSetter<T>
+
+    /** @hint this is untracked */
     peek: Accessor<T>
+
+    /**
+     * @hint this is tracked
+     *
+     * @warn do not read and write in the same spot
+     *
+     * - `signal.value++` is causing an tracked 'read' signal
+     *    and will cause infinit effect loops
+     *
+     * - use `signal.set.value++` instead,
+     *   this is equivalent to `signal.set(v => v + 1)`
+     */
     value: T
   }
 )
