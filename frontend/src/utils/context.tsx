71f7d201f0fcc0a50ec846cf06c5a0308c5bf75d
David Baierl <12718257+david-baierl@users.noreply.github.com>
2025-02-16 15:57:46 +0000

    add custom context helper

---
+++ frontend/src/utils/context.tsx
@@ -0,0 +1,48 @@
+import {
+  Context as SolidContext,
+  createContext as createSolidContext,
+  JSX,
+  ParentProps,
+  useContext,
+} from 'solid-js'
+
+import {
+  from_proxy,
+  SignalGetters,
+  to_proxy,
+} from './proxy'
+
+type Parent<P> = (props: P & ParentProps) => JSX.Element
+type Context<P> = Parent<P> & {
+  Partial: Parent<Partial<P>>
+  Context: SolidContext<P>
+  name: string
+}
+
+export function use<P>(context: Context<P>) {
+  const provided = useContext(context.Context)
+  if (!provided) throw new ReferenceError(context.name)
+  return provided
+}
+
+export function create_context<P extends object>(name: string, defaultValue: P): Context<P>
+export function create_context<P extends object>(name: string, defaultValue?: P): Context<P | undefined>
+export function create_context<P extends object>(name: string, defaultValue?: P) {
+  const Context = createSolidContext(defaultValue, { name })
+
+  const Provider: Context<P> = (props: P & ParentProps) => {
+    const { children, ...others } = from_proxy(props)
+
+    return <Context.Provider value={to_proxy(others as SignalGetters<P>)}children={children()} />
+  }
+
+  Provider.Context = Context as SolidContext<P>
+  Provider.name = name
+
+  Provider.Partial = (props: Partial<P> & ParentProps) => {
+    const prev = use(Provider)
+    return <Provider {...prev} {...props} />
+  }
+
+  return Provider
+}
